3546807f728a49488a6dbab9bd8fc01ebdd8506d
aoavzm <aoavzm@gmail.com>
2022-08-15 20:36:46 -0700

    Basic Setup

---
+++ backend/commands/npm.sh
@@ -0,0 +1,3 @@
+npm install cookie-parser cors csurf dotenv express express-async-errors helmet jsonwebtoken morgan per-env sequelize@6 sequelize-cli@6 bcryptjs
+
+npm install -D sqlite3 dotenv-cli nodemon @faker-js/faker
