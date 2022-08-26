e56741fe93a488518f71456f07a0beac1561d021
aoavzm <aoavzm@gmail.com>
2022-08-25 20:44:29 -0700

    mod

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -16,8 +16,8 @@ module.exports = (sequelize, DataTypes) => {
           { required: false, model: Review, attributes: [] }, 
           { required: false, model: Image, attributes: [] }
         ],
-        order: ['Spot.id'],
-        group: ['Spot.id', 'Images.url']
+        group: ['Spot.id', 'Images.url'],
+        order: [['id', 'ASC'], [Image, 'id', 'ASC']]
       });
       return spots;
     }
