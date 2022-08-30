460e7fb788188b2aa51dd9c102a7c9d5de77e12b
aoavzm <aoavzm@gmail.com>
2022-08-29 18:27:32 -0700

    RAW

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -64,13 +64,14 @@ module.exports = (sequelize, DataTypes) => {
     }
     static async createSpot(ownerId, { address, city, state, country, lat, lng, name, description, price }) {
       const newSpot = await Spot.create({ ownerId, address, city, state, country, lat, lng, name, description, price });
-      const spot = await Spot.findByPk(newSpot.id)
+      const spot = await Spot.scope(noUACA).findByPk(newSpot.id)
       return spot;
     }
     static async editSpot(spotId, { address, city, state, country, lat, lng, name, description, price } ) {
-      const spot = await Spot.findByPk(spotId)
-      spot.set({ address, city, state, country, lat, lng, name, description, price });
-      await spot.save();
+      const spotEdited = await Spot.findByPk(spotId)
+      spotEdited.set({ address, city, state, country, lat, lng, name, description, price });
+      await spotEdited.save();
+      const spot = await Spot.scope(noUACA).findByPk(spotEdited.id)
       return spot;
     }
     static async deleteSpot(spotId) {
@@ -223,6 +224,13 @@ module.exports = (sequelize, DataTypes) => {
         ]
       }
     },
+    scopes: {
+      noUACA: {
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        }
+      }
+    }
   });
   return Spot;
 };
